b848dcc9a859fd824d9ef20d4edb363d44d18e16
FnoizY <fares.bahamdan@gmail.com>
2025-04-03 16:53:51 +0300

    Authentication_Added_ShadcnUsed

---
+++ src/components/serverSide/login-form.tsx
@@ -0,0 +1,78 @@
+"use server"
+
+import { cn } from "@/lib/utils"
+import { FaGoogle } from "react-icons/fa";
+import { IoIosArrowBack } from "react-icons/io";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import { login } from '@/utils/actions/login'
+import { signinWithGoogle } from "@/utils/actions/signinWithOAuth";
+
+export async function LoginForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <div className="flex justify-end">
+            <Button className="">
+              <a href="/">
+                Back
+              </a>
+            </Button>
+          </div>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="example@domain.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" name="password" type="password" placeholder="******" required />
+              </div>
+              <Button type="submit" className="w-full" formAction={login}>
+                Login
+              </Button>
+              <Button variant="outline" className="w-full" formAction={signinWithGoogle} formNoValidate>
+                <FaGoogle />
+                Login with Google
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="register" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
---
+++ src/components/serverSide/register-form.tsx
@@ -0,0 +1,81 @@
+"use server"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import { register } from '@/utils/actions/register'
+
+export async function RegisterForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <div className="flex justify-end">
+            <Button className="">
+              <a href="/">
+                Back
+              </a>
+            </Button>
+          </div>
+          <CardTitle className="text-2xl">Register</CardTitle>
+          <CardDescription>
+            Enter your credentials below to create an account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+                <Label htmlFor="name">Full Name</Label>
+                <Input
+                  id="name"
+                  name="name"
+                  type="name"
+                  placeholder="Jane Doe"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="example@domain.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" name="password" type="password" placeholder="******" required />
+              </div>
+              <Button type="submit" className="w-full" formAction={register}>
+                Create an account
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <a href="login" className="underline underline-offset-4">
+                Login
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
